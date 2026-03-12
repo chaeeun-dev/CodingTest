2a9ace53a5ce74cff8a2ac2598abf67668f6a16f
im-chaeeun <dev.chaeng@gmail.com>
2026-03-12 11:21:22 +0900

    숙제 2번 해결

--- CodingTest/CodingTest/hw1-2.cpp
+++ CodingTest/CodingTest/hw1-2.cpp
@@ -12,8 +12,12 @@ int main(void)
 
 		// 층수
 		int floor = N % H;
-		// 호수
 		int room = N / H + 1;
+		if (floor == 0)	// H층일 경우 예외 처리
+		{
+			floor = H;
+			room--;		// 몫이 1이 증가되므로 빼줌
+		}
 
 		std::cout << floor * 100 + room << std::endl;
 	}
