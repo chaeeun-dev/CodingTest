a1aee1a0d1f111174e58258f41696c0efedc1dd7
chaeeun-dev <dev.chaeng@gmail.com>
2026-03-23 13:28:17 +0900

    숙제 2-2, 2-4(하는 중)

--- CodingTest/CodingTest/hw2-2.cpp
+++ CodingTest/CodingTest/hw2-2.cpp
@@ -1,2 +1,66 @@
 // 블랙잭(백준, Bruteforce, 조합)
 // https://www.acmicpc.net/problem/2798
+// C++은 Combination 라이브러리가 없으므로, 재귀함수 구현해야 함
+
+#include <iostream>
+#include <vector>
+#include <numeric>	// accumulate
+using namespace std;
+
+// lst에서 n개를 뽑는 모든 조합의 경우를 2차원 벡터로 반환
+vector<vector<int>> combi(vector<int>lst, int n)
+{
+	vector<vector<int>> ret;
+
+	if (n > lst.size())
+		return ret;
+
+	if (n == 1)
+	{
+		for (int i : lst)
+		{
+			vector<int> temp = { i };
+			ret.push_back(temp);
+		}
+	}
+	else if (n > 1)
+	{
+		for (int i = 0; i < lst.size() - n + 1; ++i)
+		{
+			vector<int> slice(lst.begin() + i + 1, lst.end());
+			for (vector<int> temp : combi(slice, n - 1))
+			{
+				vector<int> temp2 = {lst[i]};
+				temp2.insert(temp2.end(), temp.begin(), temp.end());
+				ret.push_back(temp2);
+			}
+		}
+	}
+
+	return ret;
+}
+
+int main(void)
+{
+	int N, M;
+	std::cin >> N >> M;
+
+	std::vector<int> cards;
+
+	for (int i = 0; i < N; ++i)
+	{
+		int t;
+		cin >> t;
+		cards.push_back(t);
+	}
+
+	int answer = 0;
+	for (vector<int> c : combi(cards, 3))
+	{
+		int acc = accumulate(c.begin(), c.end(), 0);
+		if (acc <= M)
+			answer = max(answer, acc);	// 최대값 갱신
+	}
+
+	cout << answer;
+}
--- CodingTest/CodingTest/hw2-4.cpp
+++ CodingTest/CodingTest/hw2-4.cpp
@@ -1,2 +1,56 @@
 ﻿// 균형 잡힌 영양소 (TUKorea, 조합) 
 // http://210.93.60.51/problem/0099
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<vector<int>> combi(vector<int>lst, int n)
+{
+	vector<vector<int>> ret;
+
+	if (n == 1)
+	{
+		for (int i : lst)
+		{
+			vector<int> temp = { i };
+			ret.push_back(temp);
+		}
+	}
+	else if (n > 1)
+	{
+		for (int i = 0; i < lst.size() - n + 1; ++i)
+		{
+			vector<int> slice(lst.begin() + i + 1, lst.end());
+			for (vector<int> temp : combi(slice, n - 1))
+			{
+				vector<int> temp2 = { lst[i] };
+				temp2.insert(temp2.end(), temp.begin(), temp.end());
+				ret.push_back(temp2);
+			}
+		}
+	}
+
+	return ret;
+}
+
+int main(void)
+{
+	int N;
+	cin >> N;
+
+	vector<vector<int>> food;	// 음식 N개<탄, 단, 지>
+	for (int i = 0; i < N; ++i)
+	{
+		std::cin >> food[i][1] >> food[i][2] >> food[i][3];
+	}
+
+	vector<int> standard(4, 0);	// 탄수, 단백질, 지방, 열량
+	cin >> standard[0] >> standard[1] >> standard[2] >> standard[3];
+
+	// 1~N 개를 뽑는 조합의 기준치 & 열량 확인하기
+	for (int i = 1; i <= N; ++i)
+	{
+		combi(foodOrder, i)
+	}
+}
